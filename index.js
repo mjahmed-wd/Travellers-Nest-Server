750838aec286119f4fe8676d1fd2b1c2172fd4c9
Md Jubair Ahmed <mjahmed.wd@gmail.com>
2021-04-17 16:01:32 +0600

    jwt token

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+// firebase
+const admin = require("firebase-admin");
+const serviceAccount = require(`./adminSDK.json`);
+// mongodb
 const ObjectId = require("mongodb").ObjectID;
 const port = 5000;
 const MongoClient = require("mongodb").MongoClient;
@@ -10,6 +14,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 app.use(bodyParser.json());
 app.use(cors());
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -33,12 +40,39 @@ client.connect((err) => {
     .db(process.env.DB_NAME)
     .collection(process.env.DB_ADMIN_COLLECTION);
 
-    app.post("/addProperty", (req, res) => {
-      const products = req.body;
-      propertyCollection.insertOne(products).then((result) => {
-        res.json(result.insertedCount);
+  // jwt token verify
+
+  const authCheck = (req, res,next) => {
+    const token = req.headers.authorization;
+    // console.log(token);
+    if (token) {
+      // console.log(token)
+      admin
+      .auth()
+      .verifyIdToken(token)
+      .then((decodedToken) => {
+        const uid = decodedToken.uid;
+        console.log(uid);
+        next()
+      })
+      .catch((error) => {
+        // Handle error
+        console.log(error)
       });
+     
+    }
+    // idToken comes from the client app
+    else {
+      res.status(401).json({ message: "Unauthorized Action" });
+    }
+  };
+
+  app.post("/addProperty", authCheck, (req, res) => {
+    const products = req.body;
+    propertyCollection.insertOne(products).then((result) => {
+      res.json(result.insertedCount);
     });
+  });
 
   app.get("/allProperties", (req, res) => {
     propertyCollection.find({}).toArray((err, documents) => {
@@ -57,24 +91,33 @@ client.connect((err) => {
   //     res.json(req.params.roll)
   //   });
 
-    app.patch("/updateProperty/:id",(req,res)=>{
-      const {name,price,address,country,description}=req.body
-      propertyCollection.updateOne({ _id: ObjectId(req.params.id) },
-      {
-        $set: {name:name, price:price, address:address,country:country,description:description }
-      })
-      .then(result=>{
-        res.send(result)
-      })
-    })
+  app.patch("/updateProperty/:id", (req, res) => {
+    const { name, price, address, country, description } = req.body;
+    propertyCollection
+      .updateOne(
+        { _id: ObjectId(req.params.id) },
+        {
+          $set: {
+            name: name,
+            price: price,
+            address: address,
+            country: country,
+            description: description,
+          },
+        }
+      )
+      .then((result) => {
+        res.send(result);
+      });
+  });
 
-    app.delete("/deleteProperty/:id", (req, res) => {
-      propertyCollection
-        .deleteOne({ _id: ObjectId(req.params.id) })
-        .then((result) => {
-          res.json(!!result.deletedCount);
-        });
-    });
+  app.delete("/deleteProperty/:id", (req, res) => {
+    propertyCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.json(!!result.deletedCount);
+      });
+  });
 
   app.post("/addOrder", (req, res) => {
     const order = req.body;
@@ -83,7 +126,6 @@ client.connect((err) => {
     });
   });
 
-  
   app.get("/orders/:email", (req, res) => {
     // const userEmail = shipment.email;
     ordersCollection
@@ -100,35 +142,36 @@ client.connect((err) => {
       res.json(result.insertedCount);
     });
   });
-  app.get("/review",(req,res)=>{
+  app.get("/review", (req, res) => {
     reviewCollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
-  })
+  });
 
   // Admin
   // All Booking
   app.get("/allPlacedOrders", (req, res) => {
     // const userEmail = shipment.email;
-    ordersCollection
-      .find({})
-      .toArray((err, documents) => {
-        res.send(documents);
-      });
+    ordersCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
   });
   // Update Order Status
-  app.patch("/updateOrderStatus/:id",(req,res)=>{
-    const {status}=req.body
+  app.patch("/updateOrderStatus/:id", (req, res) => {
+    const { status } = req.body;
     // console.log(req.body,req.params.id)
-    ordersCollection.updateOne({ _id: ObjectId(req.params.id) },
-    {
-      $set: {status:status}
-    })
-    .then(result=>{
-      res.send(result)
-    })
-  })
-// Admin Manage Section
+    ordersCollection
+      .updateOne(
+        { _id: ObjectId(req.params.id) },
+        {
+          $set: { status: status },
+        }
+      )
+      .then((result) => {
+        res.send(result);
+      });
+  });
+  // Admin Manage Section
   // Add Admin
   app.post("/addAdmin", (req, res) => {
     const adminData = req.body;
@@ -136,18 +179,18 @@ client.connect((err) => {
       res.json(result.insertedCount);
     });
   });
-  // Check If Admin or not 
+  // Check If Admin or not
   app.get("/checkAdminRole/:email", (req, res) => {
-    adminCollection.find({"email":req.params.email}).toArray((err, documents) => {
-      if(err){
-        res.sendStatus(400).send("Could not perform the search")
-      }
-      else{
-        res.json(documents)
-      }
-    });
+    adminCollection
+      .find({ email: req.params.email })
+      .toArray((err, documents) => {
+        if (err) {
+          res.sendStatus(400).send("Could not perform the search");
+        } else {
+          res.json(documents);
+        }
+      });
   });
-
 });
 
 app.listen(process.env.PORT || port);
